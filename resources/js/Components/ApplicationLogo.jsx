53c95b84d2a87cf1e17458c0ca10d803de29e272
mehluli92 <nokwaramehluli@gmail.com>
2025-03-06 00:41:44 -0500

    Logo issues

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,7 +2,7 @@ export default function ApplicationLogo(props) {
     return (
         <img
         {...props}
-        src="/images/constant/resbright-logo.jpg"
+        src="/images/constant/resbright-logo.JPG"
         />
     );
 }
