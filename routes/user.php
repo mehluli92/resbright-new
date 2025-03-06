d485cb491ddf15952b4796417722849f232d912e
mehluli92 <nokwaramehluli@gmail.com>
2025-03-06 00:15:57 -0500

    User issues

--- routes/user.php
+++ routes/user.php
@@ -9,7 +9,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/create-user', [UserController::class, 'createUserForm'])->name('create-user');
     Route::get('/user/view/{id}', [UserController::class, 'view'])->name('view-user');
     Route::put('/user/update/{id}', [UserController::class, 'update'])->name('user-update');
-    Route::get('/user-update-form/{id}', [UserController::class, 'getUpdateForm'])->name('user-update-form');
+    Route::get('/user-update-form/{id}', [UserController::class, 'getUpdateForm'])->name('user-update-f');
     Route::post('/user-store', [UserController::class, 'create'])->name('user-store');
     Route::delete('/user-delete/{id}', [UserController::class, 'delete'])->name('user-delete');
     Route::get('/user-search', [UserController::class, 'searchUser'])->name('user-search'); //just for a search returning json data
